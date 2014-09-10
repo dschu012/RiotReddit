b5cb1f01ce0b0d656abe2178c4f373cca8a27560
danny <daniel.schumacher@ed.gov>
2014-09-10 08:18:28 -0400

    spacing/notification if no comments

--- riotReddit.js
+++ riotReddit.js
@@ -12,36 +12,40 @@ function getRiotPost(i) {
 function buildNavigationSpan(post) {
 	var taglineSpan = document.createElement("span");
 	taglineSpan.style.cssFloat = "right";
-	var html = (post.index+1) + " of " + (riotPosts.length) + " Riot Posts ";
+	var html = (post.index+1) + " of " + (riotPosts.length) + " Riot Comments ";
 	var prev = getRiotPost(post.index - 1), next = getRiotPost(post.index + 1);
 	if(prev != null) {
 		//has a previous post
-		html = html + "<a href='#"+prev.id+"'>&#8249;</a>";
+		html = html + "<a href='#"+prev.id+"'> &#8249; </a>";
 	}
 	html = html + "<span class='flair flair-riot'></span>";
 	if(next != null) {
 		//has a next post
-		html = html + "<a href='#"+next.id+"'>&#8250;</a>";
+		html = html + "<a href='#"+next.id+"'> &#8250; </a>";
 	}
 	taglineSpan.innerHTML = html;
 	return taglineSpan;
 }
 
 var riotPosts = document.body.querySelectorAll('.noncollapsed .flair-riot');
+var commentsArea = document.body.querySelector('.nestedlisting');
+var firstPostDiv = document.createElement("div");
+firstPostDiv.style.textAlign = "right";
+firstPostDiv.style.padding = "10px 5px";
 if(riotPosts.length > 0) {
 	//there are riot posts
 	for(var i = 0; i < riotPosts.length; i++) {
 		var post = getRiotPost(i);
 		if(post != null) {
 			if(i === 0) {
-				var commentsArea = document.body.querySelector('.nestedlisting');
-				var firstPostDiv = document.createElement("div");
-				firstPostDiv.style.textAlign = "right";
-				firstPostDiv.innerHTML  = "First Riot Post <a href='#"+post.id+"'><span class='flair flair-riot'></span></a>";
-				commentsArea.insertBefore(firstPostDiv, commentsArea.firstChild);
+				
+				firstPostDiv.innerHTML  = "First Riot Comment <a href='#"+post.id+"'><span class='flair flair-riot'></span></a>";
+				
 			}
 			post.tagline.appendChild(buildNavigationSpan(post));
 		}
-	}
-	
+	}	
+} else {
+	firstPostDiv.innerHTML  = "No Riot Comments <span class='flair flair-riot'></span>";
 }
+commentsArea.insertBefore(firstPostDiv, commentsArea.firstChild);
